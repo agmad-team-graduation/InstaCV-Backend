90b39da1eab971b7cb264954c87ea178c45dd5c4
Osama Maher <osama392maher@gmail.com>
2025-02-25 23:43:09 +0200

    Initial commit again

---
+++ src/test/java/com/Graduation/InstaCv/InstaCvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Graduation.InstaCv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
