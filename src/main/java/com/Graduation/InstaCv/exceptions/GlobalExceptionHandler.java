093a1f5526fae169dcf030649138bc74ce48715d
Osama Maher <osama392maher@gmail.com>
2025-03-17 22:48:53 +0200

    remove ErrorController.java

--- src/main/java/com/Graduation/InstaCv/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/Graduation/InstaCv/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.Graduation.InstaCv.exceptions;
 
-import com.Graduation.InstaCv.data.dto.response.ApiErrorResponse;
+import com.Graduation.InstaCv.dto.response.ApiErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
