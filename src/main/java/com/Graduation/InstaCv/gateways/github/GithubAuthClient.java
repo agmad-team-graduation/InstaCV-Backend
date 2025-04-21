10b32c69aa0800584d3fc474ac1cbd5dd7ac7228
Youssef Morad <youssefmohammed747@gmail.com>
2025-04-21 20:48:48 +0200

    Fixed the GithubAuthClient

--- src/main/java/com/Graduation/InstaCv/gateways/github/GithubAuthClient.java
+++ src/main/java/com/Graduation/InstaCv/gateways/github/GithubAuthClient.java
@@ -2,14 +2,17 @@ package com.Graduation.InstaCv.gateways.github;
 
 import com.Graduation.InstaCv.data.dto.request.AccessTokenRequest;
 import com.Graduation.InstaCv.data.dto.response.GithubAccessTokenResponse;
-import com.Graduation.InstaCv.gateways.config.GithubFeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "githubAuthClient", url = "https://github.com", configuration = GithubFeignConfig.class)
+@FeignClient(name = "githubAuthClient", url = "https://github.com")
 public interface GithubAuthClient {
-    @PostMapping(value = "/login/oauth/access_token", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(
+            value = "/login/oauth/access_token",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            headers = "Accept=application/json"
+    )
     GithubAccessTokenResponse getAccessToken(@RequestBody AccessTokenRequest request);
 }
