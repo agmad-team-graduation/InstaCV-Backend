150ce1c399da26ff6a7d635a96bd03a1b9719d05
Youssef Morad <youssefmohammed747@gmail.com>
2025-06-14 18:45:55 +0300

    use cookie for github token.

--- src/main/java/com/Graduation/InstaCv/controller/GithubController.java
+++ src/main/java/com/Graduation/InstaCv/controller/GithubController.java
@@ -6,15 +6,23 @@ import com.Graduation.InstaCv.data.dto.response.GithubAccessTokenResponse;
 import com.Graduation.InstaCv.data.dto.response.GithubAuthLink;
 import com.Graduation.InstaCv.data.model.github.GithubProfile;
 import com.Graduation.InstaCv.service.GithubService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/api/github/test")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class GithubController {
     private final GithubService githubService;
+    @Value("${frontend.url}")
+    private String frontendUrl;
 
     @GetMapping("/authorize")
     public ResponseEntity<GithubAuthLink> authorize() {
@@ -22,12 +30,24 @@ public class GithubController {
     }
 
     @GetMapping("/callback")
-    public ResponseEntity<GithubAccessTokenResponse> callback(@RequestParam String code) {
-        return ResponseEntity.ok(githubService.getAccessToken(code));
+    public void callback(@RequestParam String code, HttpServletResponse response) throws IOException {
+        GithubAccessTokenResponse tokenResponse = githubService.getAccessToken(code);
+
+        Cookie cookie = new Cookie("github_token", tokenResponse.getAccessToken());
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(3600);
+        response.addCookie(cookie);
+
+        response.sendRedirect(frontendUrl);
     }
 
+
     @PostMapping("/profile")
-    public ResponseEntity<GithubProfile> profile(@RequestBody(required = false) GithubAccessTokenRequest githubAccessTokenRequest) {
-        return ResponseEntity.ok(githubService.getUserProfile(githubAccessTokenRequest));
+    public ResponseEntity<GithubProfile> profile(@CookieValue(value = "github_token", required = false) String githubToken) {
+        if (githubToken == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.ok(githubService.getUserProfile(new GithubAccessTokenRequest(githubToken)));
     }
 }
